57b71e13b68f17bc2851c556106c346710e3a375
Shraddha Jog <shraddha.jog@synerzip.com>
2023-04-17 17:36:52 +0530

    Assignment2: Added basic calculator app

---
+++ Calculator/src/Entry.java
@@ -0,0 +1,14 @@
+
+public class Entry {
+
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 20;
+		Maths m = new Maths();
+		m.add(a, b);
+		m.subtract(a, b);
+		m.multiply(a, b);
+		m.square(a);
+		m.cube(a);
+	}
+}
---
+++ Calculator/src/Maths.java
@@ -0,0 +1,33 @@
+
+public class Maths {
+
+	public void add(int a, int b) {
+		int result = a + b;
+		System.out.println("Addition is "+ result);
+	}
+	
+	public void subtract(int a, int b) {
+		int result = 0;
+		if(a > b) {
+			result = a - b;
+		} else {
+			result = b - a;
+		}
+		System.out.println("subtraction is "+ result);
+	}
+	
+	public void multiply(int a, int b) {
+		int result = a * b;
+		System.out.println("Multiplication is "+ result);
+	}
+	
+	public void square(int a) {
+		int result = a * a;
+		System.out.println("Square of "+ a +" is "+ result);
+	}
+	
+	public void cube(int a) {
+		int result = a * a * a;
+		System.out.println("Cube of "+ a +" is "+ result);
+	}
+}
